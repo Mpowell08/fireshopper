90e8228db2ac8e2c5128768d6a8f44fd2cac1501
Michael P <powell.r.mike@gmail.com>
2012-11-02 20:38:55 -0700

    First commit

---
+++ src/com/fireminder/fireshopper/CreateListView.java
@@ -0,0 +1,15 @@
+package com.fireminder.fireshopper;
+
+import java.util.ArrayList;
+
+import android.R.string;
+import android.app.ListActivity;
+
+public class CreateListView extends ListActivity{
+	ArrayList<string> listItems = new ArrayList<string>();
+	
+	public void createListItem(float newTotal){
+		
+	}
+
+}
